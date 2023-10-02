55f849b158964dcc6d0e77cf5f54367848d47d26
Martijn Doensen <martijn.doensen@live.nl>
2023-10-02 23:02:05 +0200

    Include friendlier errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{self, BufReader}
+    io::{self, BufReader}, process::exit,
 };
 use xml::reader::{EventReader, XmlEvent};
 
@@ -9,16 +9,22 @@ fn parse_xml_doc(file_path: &str) -> io::Result<String> {
     let file = BufReader::new(file);
     let er = EventReader::new(file);
 
-    let content: String = er.into_iter().filter_map(|event| match event {
-        Ok(XmlEvent::Characters(e)) => Some(e),
-        _ => None
-    }).collect();
+    let content: String = er
+        .into_iter()
+        .filter_map(|event| match event {
+            Ok(XmlEvent::Characters(e)) => Some(e),
+            _ => None,
+        })
+        .collect();
 
     Ok(content)
 }
 
 fn main() {
     let file_path = "data/gl2/glAccum.xhtml";
-    let doc = parse_xml_doc(file_path).expect("ERROR: could not read {file_path}");
+    let doc = parse_xml_doc(file_path).unwrap_or_else(|err| {
+        eprintln!("ERROR: Could not read {file_path}: {err}");
+        exit(1);
+    });
     println!("{doc}");
 }
